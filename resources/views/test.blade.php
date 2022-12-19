132ac83f5802627813d02979e9bd2b979ed0e368
HR0N <vivante.des@gmail.com>
2022-12-19 06:25:55 +0200

    learning. new laravel, new vite, vue

---
+++ resources/views/test.blade.php
@@ -0,0 +1,5 @@
+@extends('welcome')
+
+@section('test')
+    <example-component></example-component>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.scss'])
-    <title>Laravel</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <title>Laravel + Vue</title>
 </head>
 <body>
+    <div id="app">
+        <h2>L A R A V E L</h2>
+        @yield('test')
+    </div>
 </body>
 </html>
